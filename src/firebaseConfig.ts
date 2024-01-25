9b7b22b001cf04beff84203b175a45b378d9b9b4
guilherme ferreira <guilhermersrferreira@gmail.com>
2024-01-25 14:38:07 +0000

    add ;firebase cfg

--- src/firebaseConfig.ts
+++ src/firebaseConfig.ts
@@ -1,7 +1,13 @@
 import { FirebaseOptions } from "firebase/app";
 
 const firebaseConfig: FirebaseOptions = {
-    //TODO: ADD YOUR FIREBASE PROJECT CONFIG HERE
+    apiKey: "AIzaSyAtzEQaYKLDYf7Ad6kOrzhUn8dVehpVcnw",
+    authDomain: "wespend-c2abb.firebaseapp.com",
+    projectId: "wespend-c2abb",
+    storageBucket: "wespend-c2abb.appspot.com",
+    messagingSenderId: "537181072711",
+    appId: "1:537181072711:web:fc22e89cb20f03dc56f062",
+    measurementId: "G-LYHDR2DQ6Z"
 };
 
 export default firebaseConfig;
